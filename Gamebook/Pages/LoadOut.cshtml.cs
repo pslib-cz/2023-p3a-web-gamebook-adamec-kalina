3ff9b1b799e2833ebd076f1c6ee88a39f54f0970
kalinadam <kalinaadam75@seznam.cz>
2023-12-16 22:47:10 +0100

    new pages add

---
+++ Gamebook/Pages/LoadOut.cshtml.cs
@@ -0,0 +1,12 @@
+using Gamebook.Enums;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Gamebook.Pages;
+
+public class LoadOut : PageModel
+{
+    public void OnGet()
+    {
+        TempData["menu"] = true;
+    }
+}
---
+++ Gamebook/Pages/Map.cshtml.cs
@@ -0,0 +1,12 @@
+using Gamebook.Enums;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Gamebook.Pages;
+
+public class Map : PageModel
+{
+    public void OnGet()
+    {
+        TempData["menu"] = true;
+    }
+}
