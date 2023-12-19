b3d11416d8112cbae65ba7ef89764a14e7050c96
kalinadam <kalinaadam75@seznam.cz>
2023-12-19 09:54:17 +0100

    death screen resets game + button back to menu

--- Gamebook/Pages/Death.cshtml.cs
+++ Gamebook/Pages/Death.cshtml.cs
@@ -1,10 +1,19 @@
+using Gamebook.Interfaces;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Gamebook.Pages;
 
 public class Death : PageModel
 {
+    private readonly IGameLocationService _locationService;
+
+    public Death(IGameLocationService locationService)
+    {
+        _locationService = locationService;
+    }
+    
     public void OnGet()
     {
+        _locationService.ResetGame();
     }
 }
